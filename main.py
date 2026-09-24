97b4dce854f9f1c4f65523bd4969e50fa162b414
Rithvija <rithvijachilka@gamil.com>
2026-09-24 11:26:08 +0530

    Added while loop to repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import ollama
 
-
+#get name from input
 name =input("Enter your name: ")
 
 branch = input("Enter your branch: ")
@@ -15,14 +15,28 @@ You explain programming error messages to a 2nd year egineering student.Reply in
 Keep it under 120 words.
 '''
  
-response = ollama.chat(model="llama3.2",messages=[
-    {
-        "role": "system",
-        "content":SYSTEM
-    },
-    {
-        "role": "user",
-        "content":"Python error NameError: name 'x' is not defined"
-    }
-])
-print(response.message.content)
+while True:
+    data = input("Enter your error message or exit to stop : ")
+    if not data:
+        print("enter an error: ")
+    elif data.lower() == "exit":
+        break
+    elif data.lower() == "help":
+        print("if you enter any error message i'll resolve it if you enter exit i'll stop")
+    else:
+        try:
+            response = ollama.chat(model="llama3.2",
+            messages=[
+            {
+               "role": "system",
+               "content":SYSTEM
+            },         
+            {
+               "role": "user",
+               "content":data
+            }  
+       ])
+            print(response.message.content)
+        except Exception as e:
+            print(f"could not reach the model: {e}")
+
